e37588382c17b6ae27d176e306ab1bf562f3103d
Mykhailo <mikhail.yakubets@gmail.com>
2023-03-30 11:05:20 +0300

    Add write_data_to_file

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ def filter_data(data: list[str], keyword: str) -> list[str]:
     return [_ for _ in data if keyword in _]
 
 
+def write_data_to_file(file_path: str, data: list[str]) -> None:
+    with open(file_path, 'w') as file:
+        return file.writelines(data)
+
+
 def main() -> None:
     pass
 
