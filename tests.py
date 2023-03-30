1fe8c4ae3a09fa0409b302e297352efa2417c013
Mykhailo <mikhail.yakubets@gmail.com>
2023-03-30 11:43:32 +0300

    Test get_data_from_file

--- tests.py
+++ tests.py
@@ -1,6 +1,29 @@
-from main import get_keyword
+from typing import Generator, TextIO
+
+import pytest
+
+from . import main
+
+
+@pytest.fixture
+def data() -> list[str]:
+    return ['1111111111\n', 'sdflsBohdan554k dj']
+
+
+@pytest.fixture
+def input_file(tmp_path: str, data: list[str]) -> Generator[TextIO, None, None]:
+    with open(f'{tmp_path}/tmp_file.txt', 'w') as file:
+        file.writelines(data)
+    file = open(f'{tmp_path}/tmp_file.txt', 'r')
+    yield file
+    file.close()
 
 
 def test_get_keyword(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda x: x)
-    assert get_keyword() == 'Keyword: '
+    assert main.get_keyword() == 'Keyword: '
+
+
+def test_get_data_from_file(monkeypatch, input_file, data):
+    monkeypatch.setattr('builtins.open', lambda x: input_file)
+    assert main.get_data_from_file('') == data
